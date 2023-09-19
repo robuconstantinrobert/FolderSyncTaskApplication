01ef7afc4ad0aa1ac90f4989471849976b63efc6
robuconstantinrobert <robuconstantin20@stud.ase.ro>
2023-09-19 14:53:32 +0300

    Better menu
    Adding paths for source folder and log file and the ability to choose the synchronization time

--- TaskApplication/Program.cs
+++ TaskApplication/Program.cs
@@ -34,37 +34,70 @@ namespace TaskApplication
                 switch (action)
                 {
                     case "sync":
-                        while (true)
+                        Console.WriteLine("Choose the folder path");
+                        String path = Console.ReadLine();
+                        if (path == null)
+                        {
+                            Console.WriteLine("Path is empty");
+                            return;
+                        }
+                        else 
                         {
                             Console.Clear();
-                            Console.WriteLine("Synchronize folders");
-                            Console.WriteLine("1.Create file");
-                            Console.WriteLine("2.Copy file");
-                            Console.WriteLine("3.Delete file");
-                            Console.WriteLine("4.Exit");
-                            String action2 = Console.ReadLine();
-
-                            switch (action2)
+                            Console.WriteLine("Choose the log file path");
+                            String logPath = Console.ReadLine();
+                            if (path == null)
                             {
-                                case "create":
-                                    break;
-                                case "copy":
-                                    break;
-                                case "delete":
-                                    break;
-                                case "exit":
-                                    Console.Clear();
-                                    break;
-                                default:
-                                    Console.WriteLine("Invalid choice");
-                                    break;
+                                Console.WriteLine("Log path is empty");
+                                return;
                             }
-                            if (action2 == "exit")
+                            else 
                             {
-                                break;
+                                Console.Clear();
+                                Console.WriteLine("Choose the synchronization interval");
+                                float syncInterval = Console.Read();
+                                if (syncInterval <= 0 && syncInterval > 30)
+                                {
+                                    Console.WriteLine("The synchronization interval should be bigger than 0 seconds and lower than 30 seconds");
+                                    return;
+                                }
+                                else 
+                                {
+                                    while (true)
+                                    {
+                                        Console.Clear();
+                                        Console.WriteLine("Synchronize folders");
+                                        Console.WriteLine("1.Create file");
+                                        Console.WriteLine("2.Copy file");
+                                        Console.WriteLine("3.Delete file");
+                                        Console.WriteLine("4.Exit");
+                                        String action3 = Console.ReadLine();
+
+                                        switch (action3)
+                                        {
+                                            case "create":
+                                                break;
+                                            case "copy":
+                                                break;
+                                            case "delete":
+                                                break;
+                                            case "exit":
+                                                Console.Clear();
+                                                break;
+                                            default:
+                                                Console.WriteLine("Invalid choice");
+                                                break;
+                                        }
+                                        if (action3 == "exit")
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
                             }
                         }
-                        break;
+                        
 
                     case "exit":
                         Environment.Exit(0);
